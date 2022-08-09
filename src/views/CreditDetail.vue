6ee322e0d58377f76a26980fb79870f45a5aa364
taemin-Jang <jtm990506@gmail.com>
2022-08-09 20:15:54 +0900

    출연진 상세 페이지 수정

---
+++ src/views/CreditDetail.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <HeaderView />
+    <!-- =============== START OF MOVIE DETAIL INTRO =============== -->
+    <section
+      class="celeb-detail-intro overlay-gradient ptb100"
+      :style="{
+        background:
+          'url(' +
+          `https://image.tmdb.org/t/p/original/${credits.media?.backdrop_path})`,
+      }"
+    ></section>
+    <!-- =============== END OF MOVIE DETAIL INTRO =============== -->
+
+    <!-- =============== START OF CELEBRITY DETAIL =============== -->
+    <section class="celeb-detail pb100">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-4 col-12">
+            <div class="celeb-img">
+              <img
+                :src="`https://image.tmdb.org/t/p/w300/${credits.person?.profile_path}`"
+                alt=""
+              />
+            </div>
+          </div>
+
+          <OtherDetail :item="credits" />
+        </div>
+      </div>
+    </section>
+    <FooterView />
+  </div>
+</template>
+
+<script>
+import HeaderView from "@/components/HeaderView.vue";
+import FooterView from "@/components/FooterView.vue";
+import OtherDetail from "@/components/Credit/OtherDetail.vue";
+import axios from "axios";
+export default {
+  components: {
+    HeaderView,
+    FooterView,
+    OtherDetail,
+  },
+  data() {
+    return {
+      credits: [],
+      personId: 0,
+    };
+  },
+  async created() {
+    const credit = await axios({
+      method: "get",
+      url: `https://api.themoviedb.org/3/credit/${this.$route.params.idx}?api_key=0bb0b51dbb47771a2b73398672aac6cf&language=ko`,
+    });
+    this.credits = credit.data;
+
+    // this.personId = this.credits.person.id;
+    // console.log(this.personId);
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
