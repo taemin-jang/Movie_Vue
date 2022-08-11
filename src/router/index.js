51660ac163102dac01e23426d149bf420fabee2b
taemin-Jang <jtm990506@gmail.com>
2022-08-12 02:01:38 +0900

    HeaderSearchView 라우터 설정

--- src/router/index.js
+++ src/router/index.js
@@ -19,6 +19,33 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/DetailView.vue"),
   },
+  {
+    path: "/search/:idx",
+    name: "search",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/HeaderSearchView.vue"),
+  },
+  {
+    path: "/credit/:idx",
+    name: "credit",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/CreditListView.vue"),
+  },
+  {
+    path: "/credit/detail/:idx",
+    name: "creditDetail",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/CreditDetail.vue"),
+  },
 ];
 
 const router = new VueRouter({
