39815eae3bafa02d6abbe633f83465682b7e4246
taemin-Jang <jtm990506@gmail.com>
2022-08-14 20:28:38 +0900

    헤더 수정

--- src/components/HeaderView.vue
+++ src/components/HeaderView.vue
@@ -4,7 +4,7 @@
       <div class="container-fluid">
         <!-- ====== Start of Navbar ====== -->
         <nav class="navbar navbar-expand-lg">
-          <router-link class="navbar-brand" :to="`/`">
+          <router-link class="navbar-brand" :to="`/`" @click="reload">
             <!-- INSERT YOUR LOGO HERE -->
             <img
               src="../assets/images/logo.svg"
@@ -41,55 +41,46 @@
           <!-- ====== Start of #main-nav ====== -->
           <div class="navbar-collapse" id="main-nav">
             <!-- ====== Start of Main Menu ====== -->
-            <ul class="navbar-nav mx-auto" id="main-menu">
+            <ul
+              class="navbar-nav"
+              id="main-menu"
+              style="margin-left: auto !important"
+            >
               <!-- Menu Item -->
               <li class="nav-item dropdown">
-                <a
-                  class="nav-link dropdown-toggle"
-                  href="javascript:void(0)"
-                  data-toggle="dropdown"
-                  aria-haspopup="true"
-                  aria-expanded="false"
+                <a class="nav-link dropdown-toggle" href="javascript:void(0)"
                   >영화</a
                 >
 
                 <!-- Dropdown Menu -->
                 <ul class="dropdown-menu">
-                  <!-- Menu Item -->
                   <li>
-                    <a class="dropdown-item" href="blog-list.html"
-                      >현재 상영중인 영화</a
+                    <router-link class="dropdown-item" :to="`/nowplaying`"
+                      >현재 상영중인 영화</router-link
                     >
                   </li>
 
-                  <!-- Menu Item -->
-                  <li>
+                  <!-- <li>
                     <a class="dropdown-item" href="blog-list-fullwidth.html"
                       >개봉 예정 영화</a
                     >
                   </li>
 
-                  <!-- Menu Item -->
+                  
                   <li>
                     <a class="dropdown-item" href="blog-post-detail.html"
                       >인기 영화</a
                     >
-                  </li>
+                  </li> -->
                 </ul>
               </li>
 
               <!-- Menu Item -->
-              <li class="nav-item dropdown">
-                <a
-                  class="nav-link dropdown-toggle"
-                  href="javascript:void(0)"
-                  data-toggle="dropdown"
-                  aria-haspopup="true"
-                  aria-expanded="false"
-                  >출연자</a
-                >
+              <li class="nav-item">
+                <a class="nav-link" href="javascript:void(0)">출연자</a>
               </li>
             </ul>
+
             <!-- ====== End of Main Menu ====== -->
 
             <!-- ====== Start of Extra Nav ====== -->
@@ -103,14 +94,6 @@
                   @click="openSearch"
                 >
                   <i class="fa fa-search"></i>
-                </a>
-              </li>
-
-              <!-- Menu Item -->
-              <li class="nav-item notification-wrapper">
-                <a class="nav-link notification" href="javascript:void(0)">
-                  <i class="fa fa-globe"></i>
-                  <span class="notification-count">2</span>
                 </a>
               </li>
 
@@ -166,13 +149,15 @@ export default {
     },
     openSearch(event) {
       event.preventDefault();
-      console.log(this.searchInput);
       if (event.target._prevClass === "fa fa-search") {
         this.openSearchValue = true;
       } else {
         this.$emit("searchMovie");
         this.openSearchValue = false;
       }
+    },
+    reload() {
+      return this.$router.go();
     },
   },
 };
